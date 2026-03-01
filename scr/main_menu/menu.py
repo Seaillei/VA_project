13cd09286feab57e772fc090c2eca2718ecd90b8
Seaillei <trnkona24@sps-prosek.cz>
2026-03-01 18:08:11 +0100

    Update menu.py

--- scr/main_menu/menu.py
+++ scr/main_menu/menu.py
@@ -1,5 +1,5 @@
 import pygame
-
+ 
 FPS = 60
 current = "menu"
 
@@ -28,15 +28,14 @@ class Menu_Button():
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1 and self.rect.collidepoint(event.pos):
                 if self.action:
-                    self.action()
+                    return self.action()
+        return None
 
 def to_editor():
-    global current
-    current = "editor"
+    return "editor"
 
 def to_custom_levels():
-    global current
-    current = "custom_levels"
+    return "custom_levels"
 
 def quit_game():
     pygame.quit()
@@ -60,29 +59,29 @@ def run_menu(screen, clock):
     custom_levels_button = Menu_Button(full_width/2 - 60, 75 + 50 + 25, 120, 50, white, action = to_custom_levels, text = "CUSTOM LEVELS", font = buttons_text, text_color = black)
     quit_game_button = Menu_Button(full_width/2 - 60, 75 + 50 + 25 + 50 + 25, 120, 50, white, action = quit_game, text = "QUIT GAME", font = buttons_text, text_color = black)
 
-    menu_running = True
-
-    while menu_running:
-
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
 
-            editor_button.handle_event(event)
-            custom_levels_button.handle_event(event)
-            quit_game_button.handle_event(event)
+            result = editor_button.handle_event(event)
+            if result:
+                return result
+
+            result = custom_levels_button.handle_event(event)
+            if result:
+                return result
+
+            result = quit_game_button.handle_event(event)
+            if result:
+                return result
         
         screen.fill(gray)
 
         editor_button.draw(screen)
         custom_levels_button.draw(screen)
         quit_game_button.draw(screen)
-            
-        if current != "menu":
-            return current
 
         pygame.display.update()
         clock.tick(FPS)
-
-        pygame.display.update()
